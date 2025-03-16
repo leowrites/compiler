0c5d1d3f577c87c7f1292ad7e68f844976d63789
ubuntu <leo@liuwork.ca>
2025-03-16 03:54:38 +0000

    fix global variables

---
+++ tester/out.ll
@@ -0,0 +1,3 @@
+; ModuleID = 'out.bc'
+source_filename = "out.bc"
+target triple = "aarch64-unknown-linux-gnu"
---
+++ tester/t.sh
@@ -0,0 +1,2 @@
+../build/src/minicc ./public_tests/queen
+clang-15 output.bc ../build/minicio/libminicio.a -o output
